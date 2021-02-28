f4316ee3a62509faa5d9f2820be7d3610c345272
[A] Дубовцева Дарья <dasha.dubovtseva@mail.ru>
2021-02-28 22:57:44 +0500

    Исправлена ошибка линтера

--- src/utils/get-genres.js
+++ src/utils/get-genres.js
@@ -2,9 +2,9 @@ import {INITIAL_GENRE} from "../const";
 
 export const getGenres = (films) => {
   let genres = [INITIAL_GENRE];
-    
+
   films.forEach((film)=> {
-    !genres.includes(film.genre) && genres.push(film.genre)
+    return !genres.includes(film.genre) && genres.push(film.genre);
   });
 
   return genres;
